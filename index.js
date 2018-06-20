5990a884db66097d2178983f93e8a45bd2111458
Compilenix <compilenix@gmail.com>
2018-06-20 02:24:44 +0200

    add "content-encoding" support

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const os = require('os')
 const fs = require('fs-extra')
 const cheerio = require('cheerio')
 
+const contentEncoding = require('./src/http-content-encoding')
+
 if (!fs.existsSync('./config.js')) {
   fs.copySync('./config.example.js', './config.js')
 }
 
-let config = require('./config.js')
+const config = require('./config.js')
+const package_json = require('./package.json')
 
 /**
  * @param {http.IncomingMessage} clientRes
@@ -18,19 +21,18 @@ let config = require('./config.js')
  */
 async function getTitleFromIncomingMessage (clientRes, res) {
   return new Promise((resolve, reject) => {
-    clientRes.setEncoding('utf8')
-    if (!(clientRes.headers['content-type'].toLowerCase().startsWith('application/html') ||
-    clientRes.headers['content-type'].toLowerCase().startsWith('text/html'))) {
+    const contentType = clientRes.headers['content-type'] ? clientRes.headers['content-type'].toLowerCase() : undefined
+    if (!contentType || !(contentType.startsWith('application/html') || contentType.startsWith('text/html'))) {
       res.statusCode = 400
       clientRes.resume()
       clientRes.destroy()
       resolve('no title')
     }
 
-    let rawData = ''
+    let rawData = Buffer.alloc(0)
     clientRes.on('data', chunk => {
       if (chunk.length + rawData.length < config.maxPayloadSize) {
-        rawData += chunk
+        rawData = Buffer.concat([rawData, Buffer.from(chunk)])
       } else {
         res.statusCode = 413
         res.statusMessage = 'Response entity too large'
@@ -38,28 +40,30 @@ async function getTitleFromIncomingMessage (clientRes, res) {
         resolve('')
       }
     })
-    clientRes.on('end', () => {
+
+    clientRes.on('end', async () => {
       try {
-        const $ = cheerio.load(rawData)
+        const decoded = await contentEncoding.tryDecodeHttpResponse(clientRes, rawData)
+        const decodedBodyAsString = decoded.body.toString('utf8')
+        const $ = cheerio.load(decodedBodyAsString)
         const firstTitleElement = $('title').first()
-        if (firstTitleElement.length === 0) {
-          res.setHeader('content-type', 'text/plain')
-          res.setHeader('cache-control', 'no-cache')
-          res.setHeader('access-control-allow-origin', '*')
-          res.statusCode = 200
-          clientRes.destroy()
-          resolve('no title')
-          return
-        }
-        const title = firstTitleElement.text().trim().replace(/\n/, '').replace(/ {2}/g, '')
+        let title = ''
+
+        clientRes.destroy()
         res.setHeader('content-type', 'text/plain')
         res.setHeader('cache-control', 'no-cache')
         res.setHeader('access-control-allow-origin', '*')
         res.statusCode = 200
-        clientRes.destroy()
+
+        if (firstTitleElement.length === 0) {
+          title = 'no title'
+        } else {
+          title = firstTitleElement.text().trim().replace(/\n/, '').replace(/ {2}/g, '')
+        }
+
         resolve(title)
       } catch (e) {
-        reject(e)
+        resolve()
       }
     })
   })
@@ -83,13 +87,11 @@ function handleRequest (req, res) {
   */
   async function handleClientRequest (clientRes) {
     title = await getTitleFromIncomingMessage(clientRes, res)
-    if (!res.finished) {
-      if (title === undefined) {
-        res.statusCode = 500
-      }
-      console.dir(title)
-      res.end(title)
+    if (title === undefined) {
+      res.statusCode = 500
     }
+    console.dir(title)
+    res.end(title)
   }
 
   /**
@@ -101,15 +103,42 @@ function handleRequest (req, res) {
     res.end()
   }
 
-  const schema = clientRequest.slice(0, 5)
+  let schema = clientRequest.slice(0, 5)
+  let hostPlusRest = ''
+  if (schema === 'http/') {
+    hostPlusRest = clientRequest.slice(5)
+    schema = 'http'
+  } else if (schema === 'https') {
+    hostPlusRest = clientRequest.slice(6)
+    schema = 'https'
+  }
+
+  let url = new URL(`${schema}://${hostPlusRest}`)
+  let requestOptions = {
+    timeout: 3000,
+    protocol: url.protocol,
+    href: url.href,
+    method: 'GET',
+    host: url.host,
+    headers: {
+      'User-Agent': `${package_json.name}/${package_json.version} (${package_json.repository.url})`,
+      'Accept': 'text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.1',
+      'Accept-Language': config.preferredLanguage,
+      'Accept-Encoding': 'gzip, deflate, identity;q=0.2, *;q=0',
+      'From': config.adminContact // See: https://tools.ietf.org/html/rfc7231#section-5.5.1
+    },
+    hostname: url.hostname,
+    pathname: url.pathname,
+    path: `${url.pathname}${url.search}`,
+    search: url.search,
+    hash: url.hash
+  }
 
   let title = ''
-  if (schema === 'http/') {
-    const hostPlusRest = clientRequest.slice(5)
-    http.get(`http://${hostPlusRest}`, handleClientRequest).on('error', handleClientRequestError)
+  if (schema === 'http') {
+    http.get(requestOptions, handleClientRequest).on('error', handleClientRequestError)
   } else if (schema === 'https') {
-    const hostPlusRest = clientRequest.slice(6)
-    https.get(`https://${hostPlusRest}`, handleClientRequest).on('error', handleClientRequestError)
+    https.get(requestOptions, handleClientRequest).on('error', handleClientRequestError)
   } else {
     res.statusCode = 400
     res.end()
